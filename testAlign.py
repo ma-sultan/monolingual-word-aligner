dded64e6db90ad78d73674e429d554c00762cd70
Md Arafat Sultan <arafat.sultan@colorado.edu>
2014-03-07 11:52:01 -0700

    added functionality to the aligner -- show aligned tokens

--- testAlign.py
+++ testAlign.py
@@ -1,7 +1,10 @@
 from aligner import *
 
-sentence1 = 'For example , an electron that has been accelerated to 0.78 times the speed of light has a de Broglie wavelength of 2 pm ( 2 times 10-12 m ) , which is about 100 times smaller than the typical interatomic distance in a solid .'
-sentence2 = 'Interatomic distance in a solid is smaller than an electron .'
+sentence1 = "Four men died of an accident."
+sentence2 = "4 people was dead from a collision."
 
-print align(sentence1, sentence2)
+alignments = align(sentence1, sentence2)
+
+print alignments[0]
+print alignments[1]
 
